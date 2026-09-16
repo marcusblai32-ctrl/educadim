ef20ce53725ea05213063674b3ac800712b2b3da
marcusblai32-ctrl <marcusblai32@gmail.com>
2026-09-15 23:01:27 -0500

    Update urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'quiz'
 
 urlpatterns = [
+    # ===== WOUT PIBLIK =====
     path('', views.quiz_list, name='list'),
     path('<int:pk>/', views.quiz_detail, name='detail'),
     path('<int:pk>/commencer/', views.start_quiz, name='start'),
@@ -12,7 +13,13 @@ urlpatterns = [
     path('resultat/<int:tentative_pk>/', views.quiz_result, name='quiz_result'),
     path('question/<int:question_id>/upload-type/', views.get_question_upload_type, name='get_upload_type'),
 
-    # ===== Wout pou koreksyon staff =====
+    # ===== WOUT POU KOREKSYON STAFF =====
     path('correction/', views.tentative_list, name='tentative_list'),
     path('correction/<int:tentative_pk>/', views.corriger_tentative_view, name='corriger_tentative'),
+
+    # ===== WOUT SEKIRIZE POU MEDIA (odyo, video, image, fichye) =====
+    path('media/audio/<int:reponse_id>/', views.serve_audio_reponse, name='serve_audio'),
+    path('media/video/<int:reponse_id>/', views.serve_video_reponse, name='serve_video'),
+    path('media/image/<int:reponse_id>/', views.serve_image_reponse, name='serve_image'),
+    path('media/fichier/<int:reponse_id>/', views.serve_fichier_reponse, name='serve_fichier'),
 ]
